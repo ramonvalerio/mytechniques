959e530376b27867281ff6db34262fbd1417434b
Ramon Valerio <ramon.valerio@autodesk.com>
2022-03-17 21:27:56 -0300

    TwoSum

---
+++ Techniques.Tests/Blind75/TwoSumTests.cs
@@ -0,0 +1,68 @@
+﻿using Techniques.Tests.Blind75;
+using Xunit;
+
+namespace MyTechniques.Tests.Blind75
+{
+    public class TwoSumTests
+    {
+        [Fact]
+        public void TwoSum()
+        {
+            // arrange
+            var instance = new TwoSum();
+            var arr = new[] { 1, 2, 3 };
+
+            // act
+            var result = instance.GetIndexesOfTwoSum(arr, 4);
+
+            // assert
+            var expected = new[] { 2, 0 };
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void TwoSum_ComplexArray()
+        {
+            // arrange
+            var instance = new TwoSum();
+            var arr = new[] { 9, 2, 7, 8, 1, 3, 4 };
+
+            // act
+            var result = instance.GetIndexesOfTwoSum(arr, 4);
+
+            // assert
+            var expected = new[] { 5, 4 };
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void TwoSum_Zero()
+        {
+            // arrange
+            var instance = new TwoSum();
+            var arr = new[] { 9, 2, 7, 8, 1, 3, 4 };
+
+            // act
+            var result = instance.GetIndexesOfTwoSum(arr, 0);
+
+            // assert
+            var expected = new[] { 0, 1 };
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void TwoSum_EmptyArray()
+        {
+            // arrange
+            var instance = new TwoSum();
+            var arr = new int[9999];
+
+            // act
+            var result = instance.GetIndexesOfTwoSum(arr, 7);
+
+            // assert
+            var expected = new[] { 0, 1 };
+            Assert.Equal(expected, result);
+        }
+    }
+}
--- Techniques.Tests/Blind75/TwoSums.cs
+++ Techniques.Tests/Blind75/TwoSums.cs
@@ -1,7 +1,26 @@
-﻿namespace Techniques.Tests.Blind75
-{
-    public class TwoSums
-    {
+﻿using System.Collections.Generic;
 
+namespace Techniques.Tests.Blind75
+{
+    public class TwoSum
+    {
+        public int[] GetIndexesOfTwoSum(int[] nums, int target)
+        {
+            var n = nums.Length;
+            var map = new Dictionary<int, int>();
+
+            for (var i = 0; i < n; i++)
+            {
+                var rest = target - nums[i];
+
+                if (map.ContainsKey(rest))
+                    return new int[] { i, map[rest] };
+
+                if(!map.ContainsKey(nums[i]))
+                    map.Add(nums[i], i);
+            }
+
+            return new int[] { 0, 1 };
+        }
     }
 }
